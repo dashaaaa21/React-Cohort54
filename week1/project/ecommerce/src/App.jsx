2723807c50dd4fa883399389f90643a9e9c8dd8b
Dasha <daryna2003tk@gmail.com>
2025-12-18 09:39:35 +0100

    categories list

--- week1/project/ecommerce/src/App.jsx
+++ week1/project/ecommerce/src/App.jsx
@@ -1,7 +1,15 @@
+import categories from "./fake-data/all-categories";
+
 function App() {
     return (
         <div>
             <h1>Ecommerce Shop</h1>
+
+            <ul>
+                {categories.map((category) => (
+                    <li key={category}>{category}</li>
+                ))}
+            </ul>
         </div>
     );
 }
